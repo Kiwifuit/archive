44586146e4d2d0a2bc97ff6f2cbc7faaf27dfc8e
Kiwifuit <mahkiwi123@gmail.com>
2024-11-24 17:29:39 +0800

    feat: `get_error` crate utility

--- src/lib.rs
+++ src/lib.rs
@@ -6,3 +6,22 @@ pub mod writer;
 
 pub use core::ArchiveOptions;
 pub use writer::ArchiveWriter;
+
+use std::borrow::Cow;
+use std::ffi::CStr;
+
+fn get_error<'a>(handle: *mut archive_sys::archive, result: i32) -> Cow<'a, str> {
+    if result == 0 {
+        return Cow::from("");
+    }
+
+    let e_ptr = unsafe { archive_sys::archive_error_string(handle) };
+
+    if e_ptr.is_null() {
+        return Cow::from("unknown error");
+    }
+
+    let err = unsafe { CStr::from_ptr(e_ptr) };
+
+    err.to_string_lossy()
+}
